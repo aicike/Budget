2073f3b0200c22ad2be197c91b94e61f496c42af
gongmy <gmyao.china@gmail.com>
2014-11-27 15:29:38 +0800

    adds

--- Budget/Controllers/ProfitLossController.cs
+++ Budget/Controllers/ProfitLossController.cs
@@ -6,10 +6,15 @@ using System.Web.Mvc;
 
 namespace Budget.Controllers
 {
+    /// <summary>
+    /// 损益预算 控制器
+    /// </summary>
     public class ProfitLossController : BaseController
     {
+        //损益预算首页
         public ActionResult Index()
         {
+
             return View();
         }
 
