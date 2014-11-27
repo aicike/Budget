50b288ecd2ea70ac2d1352ebe24d8b4a2c6dc68e
aicike <176534021@qq.com>
2014-11-27 11:45:58 +0800

    first commit

---
+++ Business/MenuModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entity;
+
+namespace Business
+{
+    public class MenuModel : BaseModel<Menu>
+    {
+        public List<Menu> GetMenu()
+        {
+            return List().Where(a => a.ParentMenuID.HasValue == false).OrderBy(a=>a.Order).ToList();
+        }
+    }
+}
--- EF/Migrations/201411270316386_Update.cs
+++ EF/Migrations/201411270316386_Update.cs
@@ -2,7 +2,7 @@ namespace EF.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class Update : DbMigration
     {
         public override void Up()
@@ -23,9 +23,22 @@ namespace EF.Migrations
                 .PrimaryKey(t => t.ID)
                 .ForeignKey("dbo.Menu", t => t.ParentMenuID)
                 .Index(t => t.ParentMenuID);
-            
+
+
+            var migrationDir = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\");
+            var ddlSqlFiles = new string[] { "Initial.sql" };
+            foreach (var file in ddlSqlFiles)
+            {
+                string scriptText = System.IO.File.ReadAllText(System.IO.Path.Combine(migrationDir, file));
+                var commandTexts = scriptText.Split(new string[] { "\r\nGO\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var commandText in commandTexts)
+                {
+                    if (!String.IsNullOrWhiteSpace(commandText))
+                        Sql(commandText);
+                }
+            }
         }
-        
+
         public override void Down()
         {
             DropForeignKey("dbo.Menu", "ParentMenuID", "dbo.Menu");
