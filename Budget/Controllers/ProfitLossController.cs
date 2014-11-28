3045d23f284d69b863e1cd34e9abc6c8001aa5dd
gongmy <gmyao.china@gmail.com>
2014-11-28 10:13:13 +0800

    add

--- Budget/Controllers/ProfitLossController.cs
+++ Budget/Controllers/ProfitLossController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Business;
+using System.Data;
 
 namespace Budget.Controllers
 {
@@ -11,11 +13,48 @@ namespace Budget.Controllers
     /// </summary>
     public class ProfitLossController : BaseController
     {
-        //损益预算首页
+        //损益预算首页(公司列表)
         public ActionResult Index()
         {
+            ParticularYearModel pyearModel = new ParticularYearModel();
+            var list = pyearModel.List().OrderByDescending(a=>a.ID).ToList();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Year");
+            dt.Columns.Add("YearID");
+            dt.Columns.Add("Type"); // 0：已上报 ，1：未上报，2：未填写预算
 
-            return View();
+            foreach (var item in list)
+            {
+                DataRow row = dt.NewRow();
+                row["Year"] = item.Year + "年损益预算";
+                row["YearID"] = item.ID;
+                if (item.ProfitLoss_Main != null)
+                {
+                    var profitloss_main = item.ProfitLoss_Main.Where(a=>a.CompanyID == LoginAccount.ID);
+                    if (profitloss_main.Count() > 0)
+                    {
+                        if (profitloss_main.FirstOrDefault().IsReport)
+                        {
+                            row["Type"] = 0;
+                        }
+                        else
+                        {
+                            row["Type"] = 1;
+                        }
+                    }
+                    else
+                    {
+                        row["Type"] = 2;
+                    }
+                }
+                else
+                {
+                    row["Type"] = 2;
+                }
+
+                dt.Rows.Add(row);
+            }
+            return View(dt);
         }
 
         //预算表
